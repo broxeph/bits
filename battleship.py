62bcde455c76652a1edaa898cc9fbc928d2dbf65
Alex Ball <alex@ameryn.com>
2016-06-04 15:53:59 -0400

    Added ship hit checking, a bunch of validation

--- battleship.py
+++ battleship.py
@@ -10,11 +10,9 @@ SHIP_TYPES = {
     'Battleship': 4,
     'Carrier': 5
 }
-MAX_SHIPS = 5
-
-row_labels = string.ascii_uppercase[:10]
-col_labels = [str(col) for col in range(1, 11)]
-
+ROW_LABELS = string.ascii_uppercase
+COL_LABELS = [str(col + 1) for col in range(26)]
+DEBUG = True
 
 class CoordsError(Exception):
     pass
@@ -28,94 +26,138 @@ class ShipError(Exception):
     pass
 
 
-def parse_coords_string(coords):
+def parse_coords_string(coords_string, board_size=26):
     """Parse a coordinate string into row/column tuple."""
-    if type(coords) is not str:
+    coords_string = coords_string.strip()
+
+    if type(coords_string) is not str:
         raise CoordsError('Coordinates must be a string value.')
-    if not 2 <= len(coords) <= 3:
+    if not 2 <= len(coords_string) <= 3:
         raise CoordsError('Coordinates must be length 2 or 3.')
-    if coords[0].upper() not in row_labels:
-        raise CoordsError('Coordinates must begin with a letter in: ' + ' '.join(row_labels))
-    if coords[1:] not in col_labels:
-        raise CoordsError('Coordinates must begin with a number in: ' + ' '.join(col_labels))
+    if coords_string[0].upper() not in ROW_LABELS:
+        raise CoordsError('Coordinates must begin with a letter in: ' + ' '.join(ROW_LABELS))
+    if coords_string[1:] not in COL_LABELS:
+        raise CoordsError('Coordinates must begin with a number in: ' + ' '.join(COL_LABELS))
 
-    return (row_labels.index(coords[0].upper()), col_labels.index(coords[1:]))
+    coords = (ROW_LABELS.index(coords_string[0].upper()), COL_LABELS.index(coords_string[1:]))
+    validate_coords(coords, board_size)
+
+    return coords
 
 
-def validate_coords(coords):
-    if any([type(coords) is not tuple, len(coords) != 2, not 0 <= coords[0] <= 9, not 0 <= coords[1] <= 9]):
-        raise CoordsError('Coordinates must be a 2-tuple with values between 0 and 9.')
+def validate_coords(coords, board_size=26):
+    """Make sure coords aren't malformed and don't run off the board."""
+    if any([type(coords) is not tuple, len(coords) != 2,
+            not 0 <= coords[0] <= board_size - 1, not 0 <= coords[1] <= board_size - 1]):
+        raise CoordsError('Coordinates must be a 2-tuple with values between 0 and {}.'.format(board_size - 1))
+
+
+def coords_label(coords):
+    """Return alphanumeric label, e.g. 'B4', for given coords 2-tuple."""
+    validate_coords(coords)
+    return ROW_LABELS[coords[0]] + COL_LABELS[coords[1]]
 
 
 class Board:
-    def __init__(self, ships=None):
-        self.grid = []
+    def __init__(self, board_size=10, ships=None):
+        if not 5 <= board_size <= 26:
+            raise BoardError("Board size must be between 5 and 26.")
+        self.board_size = board_size
 
-        for row_label in row_labels:
+        self.grid = []
+        for row_label in ROW_LABELS[:self.board_size]:
             row = []
 
-            for col_label in col_labels:
+            for col_label in COL_LABELS[:self.board_size]:
                 row.append({
                     'label': row_label + col_label,
-                    'mark': '.',
+                    'mark': ' ',
                     'ship': None
                 })
-
             self.grid.append(row)
 
-        self.ships = []
         if ships is None:
             ships = []
+        self.ships = ships
+
+    def add_ships(self, ships):
         for ship in ships:
-            # Add ships to the board.
-            if len(ships) >= MAX_SHIPS:
-                raise BoardError('{} ships is too many!'.format(len(ships) + 1))
+            for point in ship.occupied_points:
+                coords = point['coords']
+                validate_coords(coords, self.board_size)
+                self.grid[coords[0]][coords[1]]['ship'] = ship
 
             self.ships.append(ship)
 
-            # TODO: Validate and update grid marks on ship additions.
-
-    def show(self):
-        print('  ' + ' '.join(col_labels))
+    def show(self, show_ships=False):
+        print('  ' + ' '.join(COL_LABELS[:self.board_size]))
         for row_index, row in enumerate(self.grid):
-            print(row_labels[row_index] + ' ' + ' '.join([point['mark'] for point in row]))
+            point_characters = []
+            for point in row:
+                point_characters.append(
+                    point['ship'].ship_type[0] if point['ship'] and show_ships else point['mark'])
+
+            print(ROW_LABELS[row_index] + ' ' + ' '.join(point_characters))
 
 
 class Ship:
     def __init__(self, begin, end, ship_type):
+        # Validate endpoints
         for point in [begin, end]:
             validate_coords(point)
         if begin[0] != end[0] and begin[1] != end[1]:
             raise ShipError('Ships cannot be placed on diagonals.')
-
         self.begin = begin
         self.end = end
 
+        # Validate ship type
         if self.length != SHIP_TYPES[ship_type]:
             raise ShipError('Invalid length {0} for ship type {1}: Must be length {2}.'.format(
                 self.length, ship_type, SHIP_TYPES[ship_type]))
         if ship_type not in SHIP_TYPES:
             raise ShipError('{} is not a valid ship type.'.format(ship_type))
-
         self.ship_type = ship_type
+
+        # Add occupied points
+        self.occupied_points = []
+        for point in range(self.length):
+            row = self.begin[0] + (point if self.begin[0] != self.end[0] else 0)
+            col = self.begin[1] + (point if self.begin[1] != self.end[1] else 0)
+            self.occupied_points.append({
+                'coords': (row, col),
+                'mark': ' '
+            })
 
     @property
     def length(self):
+        """Return the ship length as an int."""
         return max(abs(self.begin[0] - self.end[0]), abs(self.begin[1] - self.end[1])) + 1
 
+    def register_hit(self, coords):
+        """Add a hit to the appropriate coordinate."""
+        if coords not in [point['coords'] for point in self.occupied_points]:
+            raise ShipError('{0} does not contain a hit for coord {1}'.format(self, coords))
+        next(point for point in self.occupied_points if point['coords'] == coords)['mark'] = 'X'
+
     @property
-    def occupied_coords(self):
-        """Returns the coords which a ship occupies."""
-        coords = []
-        for point in range(self.length):
-            x = self.begin[0] + (point if self.begin[0] != self.end[0] else 0)
-            y = self.begin[1] + (point if self.begin[1] != self.end[1] else 0)
-            coords.append((x, y))
-        return coords
+    def hp(self):
+        """Return number of non-hit points left in the ship."""
+        return sum(1 for point in self.occupied_points if point['mark'] == ' ')
+
+    @property
+    def status(self):
+        """Return hp status or 'Sunk'."""
+        return '{0}/{1} hp'.format(self.hp, self.length) if self.hp else 'Sunk!'
 
     def __str__(self):
-        return '{0}: {1}-{2}'.format(self.ship_type, self.begin, self.end)
+        return '{0} - {1}-{2} - {3}'.format(self.ship_type, self.begin, self.end, self.status)
 
 if __name__ == '__main__':
-    board = Board()
-    board.show()
+    board = Board(board_size=10)
+    b = Ship((1, 1), (1, 4), 'Battleship')
+    p = Ship((6, 6), (7, 6), 'Patrol boat')
+    print(b)
+    print(p)
+    board.show(show_ships=True)
+    board.add_ships([b, p])
+    board.show(show_ships=True)
--- battleship_tests.py
+++ battleship_tests.py
@@ -5,7 +5,7 @@ from battleship import ShipError, CoordsError, parse_coords_string, Board, Ship
 
 class BoardTests(unittest.TestCase):
     def setUp(self):
-        self.board = Board()
+        self.board = Board(board_size=10)
 
     def test_board_length(self):
         self.assertEqual(len(self.board.grid), 10)
@@ -15,8 +15,12 @@ class BoardTests(unittest.TestCase):
     def test_board_labels(self):
         self.assertEqual(self.board.grid[0][0]['label'], 'A1')
         self.assertEqual(self.board.grid[-1][-1]['label'], 'J10')
-        self.assertEqual(self.board.grid[0][0]['mark'], '.')
-        self.assertEqual(self.board.grid[-1][-1]['mark'], '.')
+        self.assertEqual(self.board.grid[0][0]['mark'], ' ')
+        self.assertEqual(self.board.grid[-1][-1]['mark'], ' ')
+
+    def test_add_ships(self):
+        self.board.add_ships([Ship((1, 1), (1, 5), 'Carrier'), Ship((8, 8), (9, 8), 'Patrol boat')])
+        self.assertEqual(len(self.board.ships), 2)
 
 
 class ParseCoordsStringTests(unittest.TestCase):
@@ -27,7 +31,7 @@ class ParseCoordsStringTests(unittest.TestCase):
 
 class ShipSetupTests(unittest.TestCase):
     def test_ship_setup(self):
-        with self.assertRaises(CoordsError):
+        with self.assertRaises(ShipError):
             self.ship = Ship((0, 0), (0, 15), 'Carrier')
         with self.assertRaises(CoordsError):
             self.ship = Ship((0, 0), (0, 4, 0), 'Carrier')
@@ -43,7 +47,17 @@ class ShipTests(unittest.TestCase):
         self.assertEqual(self.ship.length, 5)
 
     def test_ship_occupied_coords(self):
-        self.assertEqual(self.ship.occupied_coords, [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5)])
+        self.assertEqual(self.ship.occupied_points[0], {'coords': (1, 1), 'mark': ' '})
+        self.assertEqual(self.ship.occupied_points[-1], {'coords': (1, 5), 'mark': ' '})
+
+    def test_ship_hp(self):
+        self.assertEqual(self.ship.hp, 5)
+
+    def test_ship_hit(self):
+        with self.assertRaises(ShipError):
+            self.ship.register_hit((2, 2))
+        self.ship.register_hit((1, 3))
+        self.assertEqual(self.ship.hp, 4)
 
 
 if __name__ == '__main__':
