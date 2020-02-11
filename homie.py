1da5b7fa001799039e988d9c7a08d5cc03cec548
Alex Ball <alex@ball.lol>
2020-02-10 22:11:16 -0500

    Add homie

---
+++ homie.py
@@ -0,0 +1,13 @@
+"""
+This jawn figures out when you can afford a house.
+
+In other news, something something opportunity cost index fund.
+"""
+balance = 1000
+salary_savings = 2000
+
+for month in range(30 * 12):
+    if not month % 12:
+        print(f'Year {month // 12}: {balance}')
+
+    balance += salary_savings
