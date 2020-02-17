2e37457edc19a59cbd0bacd61d5d88c64c8bbadb
Alex Ball <alex@ball.lol>
2020-02-16 20:08:18 -0500

    Format homie output in two columns

--- homie.py
+++ homie.py
@@ -3,11 +3,17 @@ This jawn figures out when you can afford a house.
 
 In other news, something something opportunity cost index fund.
 """
+COLUMN_WIDTH = 10
+YEARS = 10
 balance = 1000
 monthly_savings = 2000
 
-for month in range(30 * 12):
+print(f"Starting balance: {balance:,}")
+print(f"Monthly savings: {monthly_savings:,}\n")
+
+print("Year".ljust(COLUMN_WIDTH) + "Balance".ljust(COLUMN_WIDTH))
+for month in range(YEARS * 12):
     if not month % 12:
-        print(f'Year {month // 12}: {balance}')
+        print(str(month // 12).ljust(COLUMN_WIDTH) + f"{balance:,}".ljust(COLUMN_WIDTH))
 
     balance += monthly_savings
